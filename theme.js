26da71e7ba4645bc9e2621f82b0d212591380a94
Aiden Bai <mail.contraband@gmail.com>
2021-04-29 14:08:34 -0700

    done

---
+++ theme.js
@@ -0,0 +1,11 @@
+export default {
+  fonts: {
+    body: 'Inter, sans-serif',
+    monospace: 'monospace',
+  },
+  colors: {
+    text: 'black',
+    background: 'white',
+    primary: 'purple',
+  },
+}
